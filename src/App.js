6730e9642afc6220cbc5c556aba909f4594e0e75
Salvatore Allegra <salallegra@gmail.com>
2020-07-28 15:23:54 -0400

    Add onclick event handler to page number buttons and finish paginate logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Posts from "./Components/Posts";
+import Pagination from "./Components/Pagination";
 
 function App() {
   //setup state
@@ -24,10 +25,19 @@ function App() {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   console.log(posts);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">Blog Pagination</h1>
       <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,18 +1,22 @@
 import React from "react";
-const Pagination = ({ postsPerPage, totalPosts }) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   //Calculate the amount of pages we need
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push[i];
+    pageNumbers.push(i);
   }
   return (
-    <ul className="list-group mb-4">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
-export default Posts;
+export default Pagination;
