7f6a8a5d6ab4500fd8bc6918a6004d958b4bc17a
Salvatore Allegra <salallegra@gmail.com>
2020-07-28 13:08:09 -0400

    Set up state, add axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="App">
       <h1>Hi</h1>
