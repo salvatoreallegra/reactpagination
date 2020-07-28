5126a92d7a021ca5cfe6d450b81577bb4ad7852e
Salvatore Allegra <salallegra@gmail.com>
2020-07-28 14:27:16 -0400

    get posts from api with axios

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  //setup state
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  //get the posts on component mount
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts = res.data;
+      setLoading(false);
+    };
+  }, []);
+
+  fetchPosts(); //call Axios
+  console.log(posts);
   return (
     <div className="App">
       <h1>Hi</h1>
